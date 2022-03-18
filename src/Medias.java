16a7ed205ab19d23471dfbdd9ad8bfc3425d3168
Nadir Williams Alcalde <nadir90370@gmail.com>
2022-03-18 22:28:10 +0100

    ejemplo de Pgdp

--- src/Medias.java
+++ src/Medias.java
@@ -1,7 +1,10 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.List;
 
 //------------------------------------------First Example (One Way)-----------------------------------------------------
 class Server{
@@ -33,5 +36,45 @@ class Client {
         }catch (Exception e){
             System.out.println(e + " mensaje no enviado");
         }
+    }
+}
+
+class SocketFromPGDP{
+    private String host;
+    private String path;
+    public SocketFromPGDP(String host, String path){
+        this.host = host;
+        this.path = path;
+    }
+    public String getHost() {return host;}
+    public String getPath() {return path;}
+    //-------------------------------------
+    public String send(int port){
+        try (Socket socket = new Socket(host,port);){
+            //cliente
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            out.print("GET " +"/"+ path + " HTTP/1.1" + "\r\n");
+            out.print("Host: " + host + "\r\n");
+            out.print("\r\n");
+            out.flush();
+            //servidor
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line = "";
+            while(line != null){
+                line = in.readLine();
+                response.append(line).append("\n");
+            }
+            return response.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("nothing found");
+            return null;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        SocketFromPGDP httpRequest = new SocketFromPGDP("man1.pgdp.sse.in.tum.de", "showchar.1.html");
+        System.out.println(httpRequest.send(443));
     }
 }
