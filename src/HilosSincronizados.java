f507f6d509fa2e4c51cadf3e511db63c3018d3c1
Nadir Williams Alcalde <nadir90370@gmail.com>
2022-03-21 15:26:56 +0100

    multihilos- uso del join() parte 2

--- src/HilosSincronizados.java
+++ src/HilosSincronizados.java
@@ -1,37 +1,44 @@
 //Sincronizar es que un hilo empiece cuando recién termine el anterior
 class Sincronizado{
     public static void main(String[] args) {
-        HilosVarios hilo1 = new HilosVarios();
-        HilosVarios hilo2 = new HilosVarios();
-        HilosVarios hilo3 = new HilosVarios();
-        HilosVarios hilo4 = new HilosVarios();
-        HilosVarios hilo5 = new HilosVarios();
-        HilosVarios hilo6 = new HilosVarios();
-        HilosVarios hilo7 = new HilosVarios();
-        HilosVarios hilo8 = new HilosVarios();
-        HilosVarios hilo9 = new HilosVarios();
-        HilosVarios hilo10 = new HilosVarios();
-        HilosVarios[] hilos = {hilo1,hilo2,hilo3,hilo4,hilo5,hilo6,hilo7,hilo8,hilo9,hilo10};
-        for (HilosVarios hilo : hilos) {
+        HilosVarios hilo0 = new HilosVarios();
+        HilosVariosSincronizados hilo1 = new HilosVariosSincronizados(hilo0);
+        HilosVariosSincronizados hilo2 = new HilosVariosSincronizados(hilo1);   //hasta que el hilo1 no termine su ejecución
+        HilosVariosSincronizados hilo3 = new HilosVariosSincronizados(hilo2);   //el hilo2 no comenzará
+        HilosVariosSincronizados hilo4 = new HilosVariosSincronizados(hilo3);
+        HilosVariosSincronizados hilo5 = new HilosVariosSincronizados(hilo4);
+        HilosVariosSincronizados hilo6 = new HilosVariosSincronizados(hilo5);
+        HilosVariosSincronizados hilo7 = new HilosVariosSincronizados(hilo6);
+        HilosVariosSincronizados hilo8 = new HilosVariosSincronizados(hilo7);
+        HilosVariosSincronizados hilo9 = new HilosVariosSincronizados(hilo8);
+        HilosVariosSincronizados[] hilos = {hilo1, hilo2, hilo3, hilo4, hilo5, hilo6, hilo7, hilo8, hilo9}; //hilos sincronizados entre sí
+        for (HilosVariosSincronizados hilo : hilos) {
             hilo.start();
-            try{
-                hilo.join();                    //sin el join(), el orden de ejecución de los hilos es random
-            }catch (InterruptedException e){    //Con el join(), la ejecución es ordenada de 0 a 9
-                e.printStackTrace();
-            }
         }
-        System.out.println("terminadas las tareas XD");
-    }
+        hilo0.start();
+        System.out.println("terminadas las tareas XD");     //main liberado, se ejecuta mientras los demás
+    }                                                        // hilos están ejecutándose sincronizadamente
 }
 class HilosVarios extends Thread {
     public void run(){
         for (int i = 0; i < 2; i++) {
             System.out.println("ejecutando hilo " + getName());
-            try {
-                Thread.sleep(700);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        }
+    }
+}
+class HilosVariosSincronizados extends Thread {
+    private Thread hilo;
+    public HilosVariosSincronizados(Thread hilo){
+        this.hilo = hilo;
+    }
+    public void run(){
+        try{
+            hilo.join();                            //lo que hace es que el hilo anterior sera joineado Xd para asi
+        }catch(InterruptedException e){             //no llamar a join tantas veces
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 2; i++) {
+            System.out.println("ejecutando hilo " + getName());
         }
     }
 }
